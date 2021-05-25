1d66d85c1aef0fda64f9d68d052bab691431f5a3
wangshijun <wangshijun2010@gmail.com>
2021-05-25 18:45:47 +0800

    fix: only publish to staging node

--- tools/publish.sh
+++ tools/publish.sh
@@ -19,17 +19,6 @@ set +e
 NAME=$(cat package.json | grep name | head -n 1 |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
 VERSION=$(cat package.json | grep version | head -n 1 |  awk '{print $2}' | sed 's/"//g' | sed 's/,//g')
 
-if [ "${AWS_NODE_ENDPOINT}" != "" ]; then
-  blocklet deploy .blocklet/bundle --endpoint ${AWS_NODE_ENDPOINT} --access-key ${AWS_NODE_ACCESS_KEY} --access-secret ${AWS_NODE_ACCESS_SECRET} --skip-hooks
-  if [ $? == 0 ]; then
-    echo "deploy to ${AWS_NODE_ENDPOINT} success"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully deployed to ${AWS_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
-  else
-    echo "deploy to ${AWS_NODE_ENDPOINT} failed"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\":x: Faild to deploy ${NAME} v${VERSION} to ${AWS_NODE_ENDPOINT}\"}" ${SLACK_WEBHOOK}
-  fi
-fi
-
 if [ "${STAGING_NODE_ENDPOINT}" != "" ]; then
   blocklet deploy .blocklet/bundle --endpoint ${STAGING_NODE_ENDPOINT} --access-key ${STAGING_NODE_ACCESS_KEY} --access-secret ${STAGING_NODE_ACCESS_SECRET} --skip-hooks
   if [ $? == 0 ]; then
